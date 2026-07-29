ee2969ce9392ed70170393b8ab5a8004ec47755d
tongchong <1967034393@qq.com>
2026-07-29 10:29:37 +0800

    Fix: Remove nested git repos and update files

--- live_sql_bench_sqlite/utils/prompt_generator.py
+++ live_sql_bench_sqlite/utils/prompt_generator.py
@@ -4,7 +4,8 @@ import argparse
 import logging
 from tqdm import tqdm
 
-from prompt.baseline import assistant_prompt
+# from prompt.baseline import assistant_prompt
+from live_sql_bench_sqlite.prompt.baseline import assistant_prompt
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
